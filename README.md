d54ca2316388d12a2488d660187e5b10adcf1d38
Aryan Ramani <aryanramani67@gmail.com>
2022-11-24 18:18:44 +0530

    Final Commit - developed Web App

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 2. Jupyter Notebook
 3. VS Code
 4. Github Account
+
+
+### Steps
+
+1. Read Data
+2. Exploratory Data Analysis (Boston_Housing.ipynb)
+3. Model Creation (boston_lm.pkl)
+4. Web Application Development (app.py & flask_app)
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,12 +1,25 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from flask_app import app
+import pickle
+import numpy as np
 
-
+prediction_text = ''
+lm = pickle.load(open('/Users/notaryanramani/Documents/VSCodePython/BostonHousing/boston-housing-regression/boston_lm.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html', prediction_text='')
+    return render_template('home.html', prediction_text=prediction_text)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-        return render_template('home.html', prediction_text='AFTER SUBMIT')
+    if request.method == 'POST':
+        data = request.form.to_dict().values()
+        print(data)
+        if '' in data:
+            prediction_text = 'NaN'
+        else:
+            test = [float(x) for x in data]
+            result = lm.predict(np.array(test).reshape(1, -1))
+            prediction_text = str((float('%.2f' % result[0]) * 1000)) + ' USD'
+    return render_template('home.html', prediction_text=prediction_text)
+
