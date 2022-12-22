a06a91d051e178ff7cc5b6a9af7b6f2d70cddd17
Aryan Ramani <aryanramani67@gmail.com>
2022-12-22 19:38:52 +0530

    requirements file

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -2,9 +2,18 @@ from flask import render_template, request
 from flask_app import app
 import pickle
 import numpy as np
+import logging
+from flask.logging import default_handler
 
 prediction_text = ''
 lm = pickle.load(open('/Users/notaryanramani/Documents/VSCodePython/BostonHousing/boston-housing-regression/boston_lm.pkl', 'rb'))
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('housing-logs.log')
+logger.addHandler(file_handler)
+logger.removeHandler(default_handler)
 
 @app.route('/')
 def home():
@@ -21,6 +30,7 @@ def predict():
             test = [float(x) for x in data]
             result = lm.predict(np.array(test).reshape(1, -1))
             prediction_text = str((float('%.2f' % result[0]) * 1000)) + ' USD'
+        logger.info('Input: {} \nOutput: {}'.format(data, prediction_text))
     return render_template('home.html', prediction_text=prediction_text)
 
 @app.route('/about')
